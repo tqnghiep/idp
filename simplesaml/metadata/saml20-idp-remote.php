060213d03261e93e650a1948d8c0187027500c15
TQN <tqn@uniweb.vn>
2015-09-28 18:18:51 +0700

    Add source

---
+++ simplesaml/metadata/saml20-idp-remote.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * SAML 2.0 remote IdP metadata for simpleSAMLphp.
+ *
+ * Remember to remove the IdPs you don't use from this file.
+ *
+ * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote 
+ */
+
+/*
+ * Guest IdP. allows users to sign up and register. Great for testing!
+ */
